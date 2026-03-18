d064fa84d97880224963804c624a8d0d41fec645
Nondas Jr <jose.silva@take.net>
2026-03-18 09:27:25 -0300

    Feature 144374 Ajuste static files para collectstatic

--- config/settings.py
+++ config/settings.py
@@ -78,6 +78,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 NOME_APLICACAO = os.getenv("NOME_APLICACAO", "SME-SGP-MS-ETL")
