fbbd695d341598b4daa2b8344fa4eb7f3b6bd4b1
Max Fernandes <xaamax@gmail.com>
2026-04-29 16:32:42 -0300

    fix: mapear celery_result_backend

--- config/settings.py
+++ config/settings.py
@@ -215,6 +215,7 @@ INTERVALO_EXECUCAO_ETL_SEGUNDOS = int(
 API_KEY = os.getenv("API_KEY", "dev-key-default")
 API_KEY_HEADER = os.getenv("API_KEY_HEADER", "X-API-Key")
 CELERY_BROKER_URL = URL_KEYDB
+CELERY_RESULT_BACKEND = URL_KEYDB
 # Execução síncrona automática em testes/CI
 CELERY_TASK_ALWAYS_EAGER = os.getenv("CELERY_TASK_ALWAYS_EAGER", "1") == "1"
 
